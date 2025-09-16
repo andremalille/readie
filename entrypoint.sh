247d60de17254af3c13c25dcbf5f301791d10ff5
Andre <malii.andrii@lll.kpi.ua>
2025-09-16 17:12:08 +0300

    Revert "Deployment settings"
    This reverts commit 59aa12d6f02fa4357a0cc574885322a32683ac64.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,4 @@
-#!/bin/sh
-
-set -e
-
-echo "Waiting for database..."
-python manage.py wait_for_db
-
-echo "Applying database migrations..."
-python manage.py migrate
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-echo "Starting server..."
-gunicorn app.wsgi:application --bind 0.0.0.0:8000
+chmod +x entrypoint.sh
+git add entrypoint.sh
+git commit -m "Ensure entrypoint.sh is executable"
+git push
